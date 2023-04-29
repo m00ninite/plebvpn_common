eb01a7d4715fb10c96d05ed3e435a6468398d5bf
m00ninite <m00ninitehub@protonmail.com>
2023-04-28 21:30:00 -0700

    New error types, change order of UserAccountInfo to match sql order (for funsies)

--- types.py
+++ types.py
@@ -6,6 +6,9 @@ from typing import Union
 class PlebError(Enum):
     UNKNOWN = 1
     BAD_PORT = 2
+    NO_CONNECTION = 3
+    ACCOUNT_DOES_NOT_EXIST = 4
+    ACCOUNT_ALREADY_EXISTS = 5
 
 
 class LoginError:
@@ -16,10 +19,13 @@ class LoginError:
 
 
 class UserAccountInfo(BaseModel):
+    name: Union[str, None] = "satoshi"
+    port: Union[int, None] = 80
     hostname: Union[str, None] = "default"
-    tls_cert: Union[str, None] = "default"
+    pubkey: Union[str, None] = "default"
     agreed_price: Union[int, None] = 1000
     agreed_denomination: Union[str, None] = "sats"
+    ovpn_file: Union[str, None] = "/etc/openvpn/ovpns/dummy"
 
 
 class NewAccount(BaseModel):
